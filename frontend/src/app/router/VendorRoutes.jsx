30cea201ca27230e4b52b089faf493e70097d508
noorassoum <nourassoum@yahoo.com>
2025-12-26 22:13:22 +0200

    Update vendor routes

--- frontend/src/app/router/VendorRoutes.jsx
+++ frontend/src/app/router/VendorRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import VendorDashboard from "../../features/auth/vendor/VendorDashboard";
 import VendorBoxDetails from "../../features/vendor-box-details/VendorBoxDetails";
 import VendorReservationDetails from "../../features/vendor-reservations/VendorReservationDetails";
+import AddBox from "../../features/boxes/pages/add-box/AddBox";
 
 
 
@@ -10,6 +11,8 @@ export default function VendorRoutes() {
     <Routes>
       {/* Default vendor landing */}
       <Route path="dashboard" element={<VendorDashboard />} />
+
+      <Route path="add-box" element={<AddBox />} />
 
       {/* Vendor box details */}
       <Route path="boxes/:id" element={<VendorBoxDetails />} />
