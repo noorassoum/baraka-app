d1023e5b9352a47381e65ecd663a1375e4a7c8c6
ranimossman <ranimossman05@gmail.com>
2025-12-27 00:14:52 +0200

    /vendor/profile route added

--- frontend/src/app/router/VendorRoutes.jsx
+++ frontend/src/app/router/VendorRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import VendorDashboard from "../../features/auth/vendor/VendorDashboard";
 import VendorBoxDetails from "../../features/vendor-box-details/VendorBoxDetails";
+import VendorProfile from "../../features/vendor/VendorProfile";
 
 
 export default function VendorRoutes() {
@@ -8,6 +9,7 @@ export default function VendorRoutes() {
     <Routes>
       {/* Default vendor landing */}
       <Route path="dashboard" element={<VendorDashboard />} />
+      <Route path="profile" element={<VendorProfile />} />
 
       {/* Vendor box details */}
       <Route path="boxes/:id" element={<VendorBoxDetails />} />
